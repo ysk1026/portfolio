3be4178ee50d1a1d8843675b654ac1069c956ffc
ysk1026 <fkqoseoa@daum.net>
2020-07-23 21:04:10 +0900

    JS: Home Transparent

--- main.js
+++ main.js
@@ -46,6 +46,15 @@ contactBtn.addEventListener("click", (event) => {
   scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
 } */
 
+// Make Home slowly fade to transparent as the window scrolls down
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
